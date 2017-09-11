5e1a993de9856b68c3c1c5f01a63636ebe22d8df
Eric Erickson <eerickson@cbtnuggets.com>
2017-09-11 11:17:49 -0700

    Changed the text

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ const { addNumbers } = require('./lib/mathStuff');
 const app = express();
 
 app.get('/', (req, res) => {
-   res.send('Hello!');
+   res.send('This is NEW!!!');
 });
 
 app.get('/add/:x/plus/:y', (req, res) => {
